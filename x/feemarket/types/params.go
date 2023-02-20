ffdc42ecc029c75dead99452b1a9e87f254c1321
Randy <randy75828@gmail.com>
2023-02-20 15:28:27 +0800

    add GasLimitPerBlock and MaxBaseFee and edit DefaultBaseFee for feemarket params

--- x/feemarket/types/params.go
+++ x/feemarket/types/params.go
@@ -17,6 +17,7 @@ package types
 
 import (
 	"fmt"
+	"math/big"
 
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -27,12 +28,15 @@ import (
 var (
 	// DefaultMinGasMultiplier is 0.5 or 50%
 	DefaultMinGasMultiplier = sdk.NewDecWithPrec(50, 2)
-	// DefaultMinGasPrice is 0 (i.e disabled)
-	DefaultMinGasPrice = sdk.ZeroDec()
+	DefaultMinGasPrice      = sdk.NewDec(50_000_000_000_000)
 	// DefaultEnableHeight is 0 (i.e disabled)
 	DefaultEnableHeight = int64(0)
 	// DefaultNoBaseFee is false
-	DefaultNoBaseFee = false
+	DefaultNoBaseFee        = false
+	DefaultGasLimitPerBlock = sdk.NewInt(500_000_000)
+	DefaultInitialBaseFee   = sdk.NewInt(50_000_000_000_000)
+	// DefaultMaxBaseFee = 5 * 10^19
+	DefaultMaxBaseFee = sdk.NewIntFromBigInt(new(big.Int).Mul(new(big.Int).Exp(big.NewInt(10), big.NewInt(19), nil), big.NewInt(5)))
 )
 
 // Parameter keys
@@ -45,6 +49,8 @@ var (
 	ParamStoreKeyEnableHeight             = []byte("EnableHeight")
 	ParamStoreKeyMinGasPrice              = []byte("MinGasPrice")
 	ParamStoreKeyMinGasMultiplier         = []byte("MinGasMultiplier")
+	ParamStoreKeyGasLimitPerBlock         = []byte("GasLimitPerBlock")
+	ParamStoreKeyMaxBaseFee               = []byte("MaxBaseFee")
 )
 
 // ParamKeyTable returns the parameter key table.
@@ -62,6 +68,8 @@ func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
 		paramtypes.NewParamSetPair(ParamStoreKeyEnableHeight, &p.EnableHeight, validateEnableHeight),
 		paramtypes.NewParamSetPair(ParamStoreKeyMinGasPrice, &p.MinGasPrice, validateMinGasPrice),
 		paramtypes.NewParamSetPair(ParamStoreKeyMinGasMultiplier, &p.MinGasMultiplier, validateMinGasPrice),
+		paramtypes.NewParamSetPair(ParamStoreKeyGasLimitPerBlock, &p.GasLimitPerBlock, validateGasLimitPerBlock),
+		paramtypes.NewParamSetPair(ParamStoreKeyMaxBaseFee, &p.MaxBaseFee, validateGasLimitPerBlock),
 	}
 }
 
@@ -74,6 +82,8 @@ func NewParams(
 	enableHeight int64,
 	minGasPrice sdk.Dec,
 	minGasPriceMultiplier sdk.Dec,
+	gasLimitPerBlock sdk.Int,
+	maxBaseFee sdk.Int,
 ) Params {
 	return Params{
 		NoBaseFee:                noBaseFee,
@@ -83,6 +93,8 @@ func NewParams(
 		EnableHeight:             enableHeight,
 		MinGasPrice:              minGasPrice,
 		MinGasMultiplier:         minGasPriceMultiplier,
+		GasLimitPerBlock:         gasLimitPerBlock,
+		MaxBaseFee:               maxBaseFee,
 	}
 }
 
@@ -92,10 +104,28 @@ func DefaultParams() Params {
 		NoBaseFee:                DefaultNoBaseFee,
 		BaseFeeChangeDenominator: params.BaseFeeChangeDenominator,
 		ElasticityMultiplier:     params.ElasticityMultiplier,
-		BaseFee:                  sdkmath.NewIntFromUint64(params.InitialBaseFee),
+		BaseFee:                  DefaultInitialBaseFee,
 		EnableHeight:             DefaultEnableHeight,
 		MinGasPrice:              DefaultMinGasPrice,
 		MinGasMultiplier:         DefaultMinGasMultiplier,
+		GasLimitPerBlock:         DefaultGasLimitPerBlock,
+		MaxBaseFee:               DefaultMaxBaseFee,
+	}
+
+}
+
+// DefaultEthermintTestParams returns default evm parameters for local testing
+func DefaultEthermintTestParams() Params {
+	return Params{
+		NoBaseFee:                false,
+		BaseFeeChangeDenominator: params.BaseFeeChangeDenominator,
+		ElasticityMultiplier:     params.ElasticityMultiplier,
+		BaseFee:                  sdk.NewIntFromUint64(params.InitialBaseFee),
+		EnableHeight:             0,
+		MinGasPrice:              sdk.ZeroDec(),
+		MinGasMultiplier:         DefaultMinGasMultiplier,
+		GasLimitPerBlock:         DefaultGasLimitPerBlock,
+		MaxBaseFee:               DefaultMaxBaseFee,
 	}
 }
 
@@ -117,7 +147,15 @@ func (p Params) Validate() error {
 		return err
 	}
 
-	return validateMinGasPrice(p.MinGasPrice)
+	if err := validateMinGasPrice(p.MinGasPrice); err != nil {
+		return err
+	}
+
+	if err := validateGasLimitPerBlock(p.GasLimitPerBlock); err != nil {
+		return err
+	}
+
+	return validateMaxBaseFee(p.MaxBaseFee)
 }
 
 func validateBool(i interface{}) error {
@@ -215,5 +253,30 @@ func validateMinGasMultiplier(i interface{}) error {
 	if v.GT(sdk.OneDec()) {
 		return fmt.Errorf("value cannot be greater than 1: %s", v)
 	}
+	return nil
+}
+func validateGasLimitPerBlock(i interface{}) error {
+	value, ok := i.(sdkmath.Int)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if value.IsNegative() {
+		return fmt.Errorf("gas limit per block cannot be negative")
+	}
+
+	return nil
+}
+
+func validateMaxBaseFee(i interface{}) error {
+	value, ok := i.(sdkmath.Int)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if value.IsNegative() {
+		return fmt.Errorf("max base fee cannot be negative")
+	}
+
 	return nil
 }
