2a795f6381a8dd473f04cfa88dd3c3fc807739c1
Randy <randy75828@gmail.com>
2023-03-01 16:09:44 +0800

    Revert "edit chainId for eip712 to be cosmos chain id"
    This reverts commit 9b7efeddff6c3b2ea1fc3689300851645cde5656.

--- app/ante/eip712.go
+++ app/ante/eip712.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/migrations/legacytx"
 	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
 	ibcante "github.com/cosmos/ibc-go/v6/modules/core/ante"
+	"github.com/evmos/ethermint/x/evm/keeper"
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
 
 	ethcrypto "github.com/ethereum/go-ethereum/crypto"
@@ -167,7 +168,7 @@ func (svd LegacyEip712SigVerificationDecorator) AnteHandle(ctx sdk.Context,
 
 	// retrieve signer data
 	genesis := ctx.BlockHeight() == 0
-	chainID := ctx.ChainID()
+	chainID := keeper.EvmChainId
 
 	var accNum uint64
 	if !genesis {
