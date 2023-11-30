01b5fc7f2c2fd142f508e2763b5afd1c11def771
yan-soon <soonxiangyan@gmail.com>
2023-11-30 23:40:31 +0800

    test encoding cfg

--- app/params/encoding.go
+++ app/params/encoding.go
@@ -1,12 +1,15 @@
 package params
 
 import (
+	"cosmossdk.io/x/tx/signing"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/address"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+	"github.com/cosmos/gogoproto/proto"
 )
 
 // EncodingConfig specifies the concrete encoding types to use for a given app.
@@ -20,7 +23,20 @@ type EncodingConfig struct {
 
 func MakeEncodingConfig(modules ...module.AppModuleBasic) EncodingConfig {
 	cdc := codec.NewLegacyAmino()
-	interfaceRegistry := types.NewInterfaceRegistry()
+	interfaceRegistry, _ := types.NewInterfaceRegistryWithOptions(
+		// todo: should panic here?
+		types.InterfaceRegistryOptions{
+			ProtoFiles: proto.HybridResolver,
+			SigningOptions: signing.Options{
+				AddressCodec: address.Bech32Codec{
+					Bech32Prefix: "tswth",
+				},
+				ValidatorAddressCodec: address.Bech32Codec{
+					Bech32Prefix: "tswthvaloper",
+				},
+			},
+		},
+	)
 	codec := codec.NewProtoCodec(interfaceRegistry)
 
 	encCfg := EncodingConfig{
